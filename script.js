4bc7379f7257cc1cc6c254c4ff24b7c5c8cea423
core.editor <miriam@directimpact.online>
2023-05-26 20:44:02 +0200

    Setup hero, parallax scrolling

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// Make background header white when scroll down
 const headerElement = document.querySelector("header");
+const heroElement = document.querySelector("h1");
+const buttonElement = document.querySelector("button");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
@@ -6,4 +9,20 @@ window.addEventListener("scroll", () => {
   } else if (window.scrollY <= 50) {
     headerElement.classList.remove("header-scrolled");
   }
+  if (window.scrollY > 250) {
+    heroElement.classList.add("hero-scrolled");
+    buttonElement.classList.add("hero-scrolled");
+  } else if (window.scrollY <= 250) {
+    heroElement.classList.remove("hero-scrolled");
+    buttonElement.classList.remove("hero-scrolled");
+  }
+});
+
+// Parallax scrolling
+let heroContent = document.getElementById("hero-content");
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+
+  heroContent.style.left = value * 1.0001 + "px";
 });
